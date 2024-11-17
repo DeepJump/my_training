39cf4a2a4532bf97f3cd3ae1193ad9d7222f04ef
DeepJump <action.launcher@gmail.com>
2024-11-17 19:46:12 +0300

    Add files via upload
    homework time

---
+++ PracticalTask4.py
@@ -0,0 +1,5 @@
+completed_hw_qty=12
+spent_hrs_qty=1.5
+course_name='Python'
+one_hw_time=1.5/12
+print('Курс: ',course_name,', всего задач : ',completed_hw_qty,', затрачено часов: ',spent_hrs_qty, ',среднее время выполнения ', one_hw_time,'часа')
