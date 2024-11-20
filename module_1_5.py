3081ea40056cfb9f0186b1e96353ec92fcc06a23
DeepJump <action.launcher@gmail.com>
2024-11-20 21:23:59 +0300

    Add files via upload
    PracticalTask7

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var=(1,2,'a','b',False,55,'String')
+print('Immutable tuple:',immutable_var)
+#immutable_var[1]=66
+mutable_list=[1,2,'a','b',False,55,"string"]
+mutable_list[4]=True
+mutable_list[5]=99.3
+print('Mutable list:',mutable_list)
