a954100237d1efcb8272d9457f4479858f89a94f
DeepJump <action.launcher@gmail.com>
2024-11-25 20:27:24 +0300

    Update PracticalTest8.py
    add
    'average_grades=dict()'

--- PracticalTest8.py
+++ PracticalTest8.py
@@ -1,6 +1,6 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-average_grades={sorted(students)[0]:sum(grades[0])/len(grades[0])} #{sorted students: average grades}
+average_grades=dict()
 number_ = 0
 while number_< len(students):
   average_grades.update({sorted(students)[number_]:round(sum(grades[number_])/len(grades[number_]),2)})
