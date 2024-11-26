fb8f71601ca6ce0e7ad22d8d2b299e5e53516658
DeepJump <action.launcher@gmail.com>
2024-11-26 12:12:55 +0300

    Add files via upload
    PracticalTask10

---
+++ module_2_2.py
@@ -0,0 +1,7 @@
+number_1=input('DВведите первое число: ')
+number_2=input('DВведите второе число: ')
+number_3=input('DВведите третье число: ')
+if number_1 == number_2 or number_1 == number_3 or number_2 == number_3:
+    print(2)
+else:
+    print(0)
