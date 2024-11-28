676921434c3b68494a77c84ba642b5a17816f371
DeepJump <action.launcher@gmail.com>
2024-11-29 02:14:36 +0300

    Add files via upload
    Add condition for '3'

--- module_2_2.py
+++ module_2_2.py
@@ -1,7 +1,10 @@
 number_1=input('DВведите первое число: ')
 number_2=input('DВведите второе число: ')
 number_3=input('DВведите третье число: ')
-if number_1 == number_2 or number_1 == number_3 or number_2 == number_3:
-    print(2)
-else:
-    print(0)
+if number_1 == number_2 == number_3:
+    print(3)
+else :
+    if number_1 == number_2 or number_1 == number_3 or number_2 == number_3 :
+        print(2)
+    else :
+        print(0)
