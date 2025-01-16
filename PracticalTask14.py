880a0e317e2e3407ea38b96937da26dd78b392f2
DeepJump <action.launcher@gmail.com>
2025-01-17 01:35:40 +0300

    Add files via upload
    PPracticalTask14

---
+++ PracticalTask14.py
@@ -0,0 +1,36 @@
+
+calls = 0
+# str_k = 'ghjHJK'
+#
+# print(str_k.upper())
+
+
+def count_calls():
+    global calls
+    calls=+1
+
+
+def string_info(str_i):
+
+    tpl = (len(str_i) , str_i.upper() , str_i.lower())
+
+    # print(tpl)
+
+    return(tpl)
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+
+def iscontains(string , list_to_search):
+
+
+
+
+print(is_contains('Urbanmmmm', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
+# string_info('JHGJHGlkjlkj')
+
+
+#
